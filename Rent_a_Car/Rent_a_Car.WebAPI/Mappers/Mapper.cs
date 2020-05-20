4b7b3aaf0daed95d7d4dc47839acd30dce029d91
Almir98 <almir.tihak@edu.fit.ba>
2020-05-20 18:19:51 +0200

    Configure  automapper and swager

---
+++ Rent_a_Car/Rent_a_Car.WebAPI/Mappers/Mapper.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rent_a_Car.WebAPI.Mappers
+{
+    public class Mapper :Profile
+    {
+        public Mapper()
+        {
+
+
+
+
+
+
+
+        }
+    }
+}
--- Rent_a_Car/Rent_a_Car.WebAPI/Startup.cs
+++ Rent_a_Car/Rent_a_Car.WebAPI/Startup.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Rent_a_Car.WebAPI.Database;
 
 namespace Rent_a_Car.WebAPI
 {
@@ -26,6 +30,46 @@ namespace Rent_a_Car.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //services.AddDbContext<RentaCarContext>(c => c.UseSqlServer(Configuration.GetConnectionString("RentaCarCS")));
+            // Connection string 
+
+            var connection = @"Server =.; Database =RentaCar; Trusted_Connection = True; ";
+            services.AddDbContext<RentaCarContext>(options => options.UseSqlServer(connection));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "eProdaja API", Version = "v1" });
+
+                //c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+                //{
+                //    Name = "Authorization",
+                //    Type = SecuritySchemeType.Http,
+                //    Scheme = "basic",
+                //    In = ParameterLocation.Header,
+                //    Description = "Basic Authorization header using the Bearer scheme."
+                //});
+
+                //c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                //{
+                //    {
+                //        new OpenApiSecurityScheme
+                //        {
+                //            Reference = new OpenApiReference
+                //            {
+                //                Type = ReferenceType.SecurityScheme,
+                //                Id = "basic"
+                //            }
+                //        },
+                //        new string[] {}
+                //    }
+                //});
+            });
+
+            //Automapper
+            services.AddAutoMapper(typeof(Startup));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +80,16 @@ namespace Rent_a_Car.WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseAuthentication();
+
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
