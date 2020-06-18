de32d649e32c62ca250fff49cf513fabac13ac82
Almir98 <almir.tihak@edu.fit.ba>
2020-06-19 00:59:44 +0200

    Authentification in Xamarin completed

---
+++ Rent_a_Car/RentACar.Mobile/RentACar.Mobile/APIService.cs
@@ -0,0 +1,109 @@
+﻿using Flurl.Http;
+using Flurl;
+using RentaCar.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RentACar.Mobile
+{
+    public class APIService
+    {
+        public static string Username { get; set; }
+        public static string Password { get; set; }
+        private readonly string _route;
+
+#if DEBUG 
+        private string _apiUrl = "http://localhost:51887/api";
+#endif
+#if RELEASE
+        private string _apiUrl = "https://mywebsite.azure.com/api/";
+#endif
+
+        public APIService(string route)
+        {
+            _route = route;
+        }
+
+        public async Task<T> Get<T>(object search)
+        {
+            var url = $"{_apiUrl}/{_route}";
+
+            try
+            {
+                if (search != null)
+                {
+                    url += "?";
+                    url += await search.ToQueryString();
+                }
+
+                return await url.WithBasicAuth(Username, Password).GetJsonAsync<T>();
+            }
+            catch (FlurlHttpException ex)
+            {
+                if (ex.Call.HttpStatus == System.Net.HttpStatusCode.Unauthorized)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error", "Wrong username or password", "Try again");
+                }
+                throw;
+            }
+        }
+
+        public async Task<T> GetById<T>(object id)
+        {
+            var url = $"{_apiUrl}/{_route}/{id}";
+
+            return await url.WithBasicAuth(Username, Password).GetJsonAsync<T>();
+        }
+
+        public async Task<T> Insert<T>(object request)
+        {
+            var url = $"{_apiUrl}/{_route}";
+
+            try
+            {
+                return await url.WithBasicAuth(Username, Password).PostJsonAsync(request).ReceiveJson<T>();
+            }
+            catch (FlurlHttpException ex)
+            {
+                var errors = await ex.GetResponseJsonAsync<Dictionary<string, string[]>>();
+
+                var stringBuilder = new StringBuilder();
+                foreach (var error in errors)
+                {
+                    stringBuilder.AppendLine($"{error.Key}, ${string.Join(",", error.Value)}");
+                }
+
+                await Application.Current.MainPage.DisplayAlert("Greška", stringBuilder.ToString(), "OK");
+                return default(T);
+            }
+        }
+
+        public async Task<T> Update<T>(int id, object request)
+        {
+            try
+            {
+                var url = $"{_apiUrl}/{_route}/{id}";
+
+                return await url.WithBasicAuth(Username, Password).PutJsonAsync(request).ReceiveJson<T>();
+            }
+            catch (FlurlHttpException ex)
+            {
+                var errors = await ex.GetResponseJsonAsync<Dictionary<string, string[]>>();
+
+                var stringBuilder = new StringBuilder();
+                foreach (var error in errors)
+                {
+                    stringBuilder.AppendLine($"{error.Key}, ${string.Join(",", error.Value)}");
+                }
+
+                await Application.Current.MainPage.DisplayAlert("Greška", stringBuilder.ToString(), "OK");
+                return default(T);
+            }
+
+        }
+    }
+}
+
---
+++ Rent_a_Car/RentACar.Mobile/RentACar.Mobile/ViewModels/LoginViewModel.cs
@@ -0,0 +1,53 @@
+﻿using RentACar.Mobile.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RentACar.Mobile.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private readonly APIService _service = new APIService("Customer");
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(async () => await Login());
+        }
+
+        string _username = string.Empty;
+        public string Username
+        {
+            get { return _username; }
+            set { SetProperty(ref _username, value); }
+        }
+
+        string _password = string.Empty;
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        public ICommand LoginCommand { get; set; }
+
+        async Task Login()
+        {
+            IsBusy = true;
+            APIService.Username = Username;
+            APIService.Password = Password;
+
+            try
+            {
+                await _service.Get<dynamic>(null);
+                Application.Current.MainPage = new MainPage();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
