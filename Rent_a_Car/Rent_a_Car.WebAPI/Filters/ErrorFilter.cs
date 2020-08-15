2e83dd53fab2ef93fd9d628c2354fc064ad96ac1
Almir <almir.tihak98@hotmail.com>
2020-08-15 05:05:40 +0200

    docker configuration part one

--- Rent_a_Car/Rent_a_Car.WebAPI/Filters/ErrorFilter.cs
+++ Rent_a_Car/Rent_a_Car.WebAPI/Filters/ErrorFilter.cs
@@ -1,32 +1,32 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.Filters;
-//using Rent_a_Car.WebAPI.Exceptions;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Net;
-//using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Rent_a_Car.WebAPI.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 
-//namespace Rent_a_Car.WebAPI.Filters
-//{
-//    public class ErrorFilter: ExceptionFilterAttribute
-//    {
-//        public override void OnException(ExceptionContext context)
-//        {
-//            if (context.Exception is UserException)
-//            {
-//                context.ModelState.AddModelError("ERROR", context.Exception.Message);                       // Client Exception
-//                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-//            }
-//            else
-//            {
-//                context.ModelState.AddModelError("ERROR", "Greška na serveru");                             // Server Exception
-//                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-//            }
+namespace Rent_a_Car.WebAPI.Filters
+{
+    public class ErrorFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            if (context.Exception is UserException)
+            {
+                context.ModelState.AddModelError("ERROR", context.Exception.Message);                       // Client Exception
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+            else
+            {
+                context.ModelState.AddModelError("ERROR", "Greška na serveru");                             // Server Exception
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
 
-//            var list = context.ModelState.Where(x => x.Value.Errors.Count > 0).ToDictionary(x => x.Key, y => y.Value.Errors.Select(z => z.ErrorMessage));
+            var list = context.ModelState.Where(x => x.Value.Errors.Count > 0).ToDictionary(x => x.Key, y => y.Value.Errors.Select(z => z.ErrorMessage));
 
-//            context.Result = new JsonResult(list);
-//        }
-//    }
-//}
+            context.Result = new JsonResult(list);
+        }
+    }
+}
--- Rent_a_Car/Rent_a_Car.WebAPI/Startup.cs
+++ Rent_a_Car/Rent_a_Car.WebAPI/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-//using Rent_a_Car.WebAPI.Filters;
+using Rent_a_Car.WebAPI.Filters;
 using Rent_a_Car.WebAPI.Interface;
 using Rent_a_Car.WebAPI.Service;
 using RentaCar.Data.Requests.Booking;
@@ -37,11 +37,11 @@ namespace Rent_a_Car.WebAPI
 
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddMvc(x => x.Filters.Add<ErrorFilter>());         // Filter for errors
+            services.AddMvc(x => x.Filters.Add<ErrorFilter>());        
 
             services.AddControllers();
 
-            services.AddDbContext<RentaCarContext>(c => c.UseSqlServer(Configuration.GetConnectionString("RentaCarCS")));       // Connection string
+            services.AddDbContext<RentaCarContext>(c => c.UseSqlServer(Configuration.GetConnectionString("RentaCarCS")));    
 
             services.AddControllers().AddNewtonsoftJson(options =>
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
